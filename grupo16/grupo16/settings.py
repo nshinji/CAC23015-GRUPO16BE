05283d5265d43d7cb31da81389544909d55f4409
VWAR785 <shinji.nicolas@gmail.com>
2023-07-05 08:19:41 -0300

    update anderson

--- grupo16/grupo16/settings.py
+++ grupo16/grupo16/settings.py
@@ -116,15 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_ROOT = '.'
-
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-# ]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "grupo16/static"),
+STATIC_URL= 'static/'
+STATICFILES_DIRS=[
+BASE_DIR  / 'static'
 ]
-STATIC_URL =  'grupo16/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
